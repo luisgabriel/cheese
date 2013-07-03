abd4768e43f0b18e54515ee4df297573b5d8f72c
Luis Gabriel Lima <luis.gabriel@openbossa.org>
2013-07-03 11:34:28 -0300

    Implementing game over

--- js/game.js
+++ js/game.js
@@ -3,9 +3,12 @@ var Game = function () {
     this._context = null;
     this._canvas = null;
     this._inputs = null;
+    this._currentLevel = null;
 
     this._bg = new Image();
     this._bg.src = "images/bg.png";
+
+    this._running = false;
 };
 
 Game.TICK = 30;
@@ -17,11 +20,24 @@ Game.prototype.init = function () {
     this._inputs = [];
     this._canvas.addEventListener("click", this.onClicked.bind(this));
 
+    this._currentLevel = Levels[1];
     this._cheese = new Cheese(0, this._canvas.height - 469);
-    this._cheese.loadLevel(Levels[2]);
+    this._cheese.loadLevel(this._currentLevel);
+};
+
+Game.prototype.reset = function () {
+    this._inputs = [];
+    this._cheese = new Cheese(0, this._canvas.height - 469);
+    this._cheese.loadLevel(this._currentLevel);
 };
 
 Game.prototype.onClicked = function (e) {
+    if (!this._running) {
+        this.reset();
+        this.start();
+        return;
+    }
+
     var click = {
         x: e.clientX,
         y: e.clientY
@@ -30,15 +46,30 @@ Game.prototype.onClicked = function (e) {
 };
 
 Game.prototype.start = function () {
+    this._running = true;
     this._mainLoop();
 };
 
 Game.prototype._mainLoop = function () {
+    if (!this._running)
+        return;
+
+    if (this._cheese.hp < this._currentLevel.minScore) {
+        this._gameOver();
+        return;
+    }
+
     this._processInput();
     this._update();
     this._draw();
 
     setTimeout(this._mainLoop.bind(this), Game.TICK);
+};
+
+Game.prototype._gameOver = function () {
+    this._running = false;
+    Utils.drawText(this._context, "GAME OVER!", 74, 200, 50);
+    Utils.drawText(this._context, "Click somewhere to restart the game.", 110, 220, 15);
 };
 
 Game.prototype._processInput = function () {
@@ -53,4 +84,5 @@ Game.prototype._update = function () {
 Game.prototype._draw = function () {
     this._context.drawImage(this._bg, 0, 0);
     this._cheese.draw(this._context);
+    Utils.drawText(this._context, "LIFE: " + this._cheese.hp, 5, 20);
 };
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,13 @@
 var Utils = {
     getRandomInt: function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    drawText: function (context, text, x, y, size, color) {
+        context.lineWidth = 1;
+        context.fillStyle = color || "#FFFF00";
+        context.lineStyle = "#FFFFFF00";
+        context.font = (size || 18) + "px sans-serif";
+        context.fillText(text, x, y);
     }
 };
